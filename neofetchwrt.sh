3c460fc393c8007b33cd3bed383188f34074b9dd
Amirul Islam <AmirulAndalibGit@gmail.com>
2022-10-28 21:44:01 +0600

    Update neofetchwrt.sh

--- neofetchwrt.sh
+++ neofetchwrt.sh
@@ -1,8 +1,14 @@
-echo "Downloading neofetch"
-logger "installer[$$] : Downloading neofetch"
+echo -e "\e[32m[INFO]: Downloading Neofetch.\e[0m"
 wget -O /bin/neofetch "https://raw.githubusercontent.com/dylanaraps/neofetch/master/neofetch"
-chmod +x /bin/neofetch
-echo "setting up neofetch..."
+echo -e "\e[32m[INFO]: Installing bash.\e[0m"
 opkg install bash
+echo -e "\e[32m[INFO]: Installing Neofetch.\e[0m"
+chmod -R +x /bin/neofetch
+echo -e "\e[32m[INFO]: Backing up Neofetch directories.\e[0m"
+cat << EOF >> /etc/sysupgrade.conf
+/bin/neofetch
+EOF
+echo -e "\e[32m[INFO]: Making Neofetch to show up in the openwrt ssh login banner.\e[0m"
 sed -i 's/\[ -f \/etc\/banner \] && cat \/etc\/banner/#&/' /etc/profile
 sed -i 's/\[ -n "$FAILSAFE" \] && cat \/etc\/banner.failsafe/& || \/bin\/neofetch/' /etc/profile
+
